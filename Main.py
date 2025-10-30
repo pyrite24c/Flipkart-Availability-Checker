8b750885f2c9bce6f35d7a033453b4e105a73ded
pyrite24c <sanwasajid2@gmail.com>
2025-10-30 14:59:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -35,3 +35,8 @@ def check(url: str, pincodes: str):
     for pin in pins:
         results[pin] = check_availability(product_id, pin.strip())
     return results
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
